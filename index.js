e4bfa96a2183d596ee6d6e742ad4074263cda347
rimasultana <rimasultana20225@gmail.com>
2024-03-27 22:30:59 +0600

    callback

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+function myDisplayer(some){
+    console.log(some);
+}
+
+
+
+function myFirst(){
+    myDisplayer("Hello");
+}
+function mySeconsd(){
+    myDisplayer("Goodbye");
+}
+
+myFirst();
+mySeconsd();
+
+
+function disply(some){
+    console.log(some);
+}
+
+function claculator(num1, num2, myCallback){
+    let sum = num1 + num2;
+    myCallback (sum);
+}
+
+let result = claculator(10, 10, disply);
